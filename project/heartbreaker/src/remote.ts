dbb4740bf4d40b75fce3f45373cad19fd855cf61
rikka <admin@rikka.cc>
2022-12-04 11:22:49 +0800

    feat: 🎸 完善notification

--- project/heartbreaker/src/remote.ts
+++ project/heartbreaker/src/remote.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Rikka
  * @Date: 2022-12-02 21:45:04
- * @LastEditTime: 2022-12-02 22:34:41
+ * @LastEditTime: 2022-12-04 11:21:04
  * @LastEditors: Rikka
  * @Description:
  * @FilePath: \stark\project\heartbreaker\src\remote.ts
@@ -9,9 +9,10 @@
 import { App } from "vue";
 import Notifications from "@kyvg/vue3-notification";
 import { router_list } from "./router";
+import velocity from "velocity-animate";
 
 function register(app: App<Element>) {
-  app.use(Notifications);
+  app.use(Notifications, { velocity });
 }
 
 export { register, router_list };
