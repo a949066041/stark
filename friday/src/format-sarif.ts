bb0cd1f1aa4a389a1b52942709732ae793fa20f0
rikka <admin@rikka.cc>
2022-12-26 21:32:21 +0800

    feat: 🎸 fix format sarif

--- friday/src/format-sarif.ts
+++ friday/src/format-sarif.ts
@@ -1,13 +1,15 @@
 /*
  * @Author: Rikka
  * @Date: 2022-12-26 20:56:41
- * @LastEditTime: 2022-12-26 21:17:52
+ * @LastEditTime: 2022-12-26 21:31:21
  * @LastEditors: Rikka
  * @Description:
  * @FilePath: \stark\friday\src\format-sarif.ts
  */
-import fsExtra, { writeFileSync } from "fs-extra";
+import fsExtra from "fs-extra";
 import { resolve } from "path";
+
+const { readFileSync, writeFileSync } = fsExtra;
 
 interface SarifRunItem {
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -17,11 +19,10 @@ interface SarifRunItem {
   results: Array<any>;
 }
 
-const { readFileSync } = fsExtra;
-const sarif_path = readFileSync(resolve("../", "eslint-results.sarif"));
-const sarif_file = JSON.parse(sarif_path.toString());
+const sarif_path = resolve("../", "eslint-results.sarif");
+const sarif_file = JSON.parse(readFileSync(sarif_path).toString());
 // eslint-disable-next-line unicorn/no-array-for-each
 sarif_file.runs.forEach((item: SarifRunItem) => {
   item.results = item.results.filter((result) => result.suppressions === undefined);
 });
-writeFileSync(JSON.stringify(sarif_file, null, 2), sarif_path);
+writeFileSync(sarif_path, JSON.stringify(sarif_file, null, 2));
